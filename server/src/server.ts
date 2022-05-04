4cc07cb9c1e30df4cb038c263c5e59fa87dab8ea
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-05-04 15:49:19 -0300

    feat: added nodemailer, configured and tested

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import nodemailer from "nodemailer";
+
 import { prisma } from "./prisma";
 
 const port = 3333;
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e4582dfd2e2994",
+    pass: "04bca30442a7f5",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -16,7 +27,17 @@ app.post("/feedbacks", async (req, res) => {
       screenshot,
     },
   });
-
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Gustavo Henrique <gustavo._henrique@hotmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
   return res.status(201).json({ data: feedback });
 });
 
