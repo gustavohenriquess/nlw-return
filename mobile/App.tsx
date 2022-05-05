016086ade9ab26d636ec4a9fdd5ba78e6952a93b
Gustavo Henrique <gustavo._henrique@hotmail.com>
2022-05-05 12:41:23 -0300

    feat: use of fonts and load

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium 
+} from '@expo-google-fonts/inter';
+
+
+
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+
   return (
     <View style={{
       flex: 1,
